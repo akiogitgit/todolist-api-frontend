4944e070fd695fc32b3a35903c9713a31093211d
Akio Tanaka <akimaron2828@icloud.com>
2022-05-15 16:52:55 +0900

    feat: user update, delete(ログインができてないから)

---
+++ src/pages/myPage.tsx
@@ -0,0 +1,82 @@
+import axios from 'axios'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { useState } from 'react'
+import Layout from '../components/Layout'
+
+const MyPage: NextPage = () => {
+  // ログインをheaderでして、sessionにでも格納して
+  // 自分のuser.idを使えるようにする
+
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+  const deleteUser = (id: number) => {
+    axios
+      .delete(String(process.env.NEXT_PUBLIC_DEV_API_URL) + `users/${id}`)
+      .then((res) => {
+        console.log('delete user', res.data)
+      })
+  }
+
+  const update = (e: React.FormEvent<HTMLFormElement>, id: number) => {
+    axios
+      .patch(String(process.env.NEXT_PUBLIC_DEV_API_URL) + `users/${id}`, {
+        user: {
+          name: name,
+          password: password, //pass
+          password_confirmation: passwordConfirmation,
+        },
+      })
+      .then((res) => {
+        console.log('update user', res.data)
+      })
+    e.preventDefault()
+  }
+
+  return (
+    <Layout>
+      <div>mypage</div>
+
+      <form onSubmit={(e) => update(e, 3)}>
+        <input
+          type='text'
+          name='name'
+          placeholder='名前'
+          className='border border-black'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br></br>
+        <input
+          type='password'
+          name='password'
+          placeholder='パスワード'
+          className='border border-black'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br></br>
+        <input
+          type='password'
+          name='password_confirmation'
+          placeholder='確認パスワード'
+          className='border border-black'
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <br></br>
+        <button type='submit'>更新</button>
+      </form>
+
+      <button
+        onClick={() => deleteUser(3)}
+        className='px-1 border-2 border-red-500 bg-red-500 rounded text-white font-bold duration-300 hover:bg-white hover:text-red-500 hover:rotate-[360deg] hover:scale-[1.2] hover:skew-x-12 '
+      >
+        ユーザーを削除
+      </button>
+    </Layout>
+  )
+}
+export default MyPage
---
+++ src/pages/register.tsx
@@ -0,0 +1,67 @@
+import axios from 'axios'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { useState } from 'react'
+const Register: NextPage = () => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+  const createUser = (e: React.FormEvent<HTMLFormElement>) => {
+    axios
+      .post(String(process.env.NEXT_PUBLIC_DEV_API_URL) + 'users', {
+        user: {
+          name: name,
+          password: password,
+          password_confirmation: passwordConfirmation,
+        },
+      })
+      .then((res) => {
+        console.log('create user', res.data)
+      })
+      .catch((err) => {
+        console.log('registration error', err)
+      })
+    console.log('create user')
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <Link href='/'>index</Link>
+      <h1>ユーザー登録ページ</h1>
+
+      <section>
+        <form onSubmit={createUser}>
+          <input
+            type='text'
+            name='name'
+            placeholder='名前'
+            className='border border-black'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='パスワード'
+            className='border border-black'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type='password'
+            name='password_confirmation'
+            placeholder='確認パスワード'
+            className='border border-black'
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+          <button type='submit'>作成</button>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default Register
